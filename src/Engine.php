48882f61822ead7be6ffaa75f0d34246d28ddcab
Vladimir Ugarov <graciadan@rambler.ru>
2022-07-28 23:57:09 +0300

    add step 5

---
+++ src/Engine.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Brain\Engine;
+
+use function cli\line;
+use function cli\prompt;
+
+const ROUNDS_COUNT = 3;
+
+function runGame($condition, $gameData)
+{
+    line('Welcome to the Brain Game!');
+    line($condition);
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    foreach ($gameData as [$question, $correctAnswer]) {
+        line("Question: $question");
+        $answer = prompt("Your answer");
+        if ($answer !== $correctAnswer) {
+            line("'{$answer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'.");
+            line("Let's try again, {$name}!");
+            return;
+        }
+        line("Correct!");
+    }
+    line("Congratulations, {$name}!");
+}
---
+++ src/games/Even.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Brain\Games\Even;
+
+use function Brain\Engine\runGame;
+
+use const Brain\Engine\ROUNDS_COUNT;
+
+const DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no"';
+
+function isEven($value)
+{
+    return $value % 2 == 0 ? true : false;
+}
+
+function run()
+{
+    $gameData = [];
+    for ($i = 0; $i < ROUNDS_COUNT; $i++) {
+        $randomNum = mt_rand(1, 100);
+        $currentQuestion = (string) $randomNum;
+        $currentAnswer = isEven($randomNum) ? 'yes' : 'no';
+        //$currentQuestion = (string) $randomNum;
+        $gameData[] = [$currentQuestion, $currentAnswer];
+    }
+    runGame(DESCRIPTION, $gameData);
+}
