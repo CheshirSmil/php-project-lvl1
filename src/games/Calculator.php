70c93b45c596d1ca128351d3d340e30f2402dac6
Vladimir Ugarov <graciadan@rambler.ru>
2022-07-29 19:36:06 +0300

    add step 6

---
+++ src/games/Calculator.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Brain\Games\Calculator;
+
+use function Brain\Engine\runGame;
+
+use const Brain\Engine\ROUNDS_COUNT;
+
+const DESCRIPTION = 'What is the result of the expression?';
+
+function calculate($first, $second, $sign)
+{
+    switch ($sign) {
+        case '*':
+            return $first * $second;
+        case '+':
+            return $first + $second;
+        default:
+            throw new \Exception('Unknown operation symbol ' . $sign);
+    }
+}
+
+function run()
+{
+    $gameData = [];
+    $signs = ['+', '*'];
+
+    for ($i = 0; $i < ROUNDS_COUNT; $i++) {
+        $firstOperand = mt_rand(1, 100);
+        $secondOperand = mt_rand(1, 100);
+        $signNum = array_rand($signs);
+        $sign = $signs[$signNum];
+
+        $currentAnswer = (string) calculate($firstOperand, $secondOperand, $sign);
+        $currentQuestion = "{$firstOperand} {$sign} {$secondOperand}";
+
+        $gameData[] = [$currentQuestion, $currentAnswer];
+    }
+
+    runGame(DESCRIPTION, $gameData);
+}
