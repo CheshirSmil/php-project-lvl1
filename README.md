d7063775d62b4db265855db852b5249471a1e96e
Vladimir Ugarov <graciadan@rambler.ru>
2022-07-30 00:28:20 +0300

    add step 7

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 <a href="https://codeclimate.com/github/CheshirSmil/php-project-lvl1/maintainability"><img src="https://api.codeclimate.com/v1/badges/4a45876e296a624651ba/maintainability" /></a>
 https://asciinema.org/a/XG8VgcaB6XhGCyZcdSyWr8zqR
 https://asciinema.org/a/uuysjA89Zg7E0DMkuBX0sRClX
+https://asciinema.org/a/61pyCJLt8yLTtK9wRH6iDj8Ed
---
+++ src/games/Gcd.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Brain\Games\Gcd;
+
+use function Brain\Engine\runGame;
+
+use const Brain\Engine\ROUNDS_COUNT;
+
+const DESCRIPTION = 'Find the greatest common divisor of given numbers.';
+
+function getGcd($a, $b)
+{
+    while ($a != 0 && $b != 0) {
+        if ($a > $b) {
+            $a = $a % $b;
+        } else {
+            $b = $b % $a;
+        }
+    }
+
+    return $a + $b;
+}
+
+function run()
+{
+    $gameData = [];
+
+    for ($i = 0; $i < ROUNDS_COUNT; $i++) {
+        $firstOperand = mt_rand(1, 100);
+        $secondOperand = mt_rand(1, 100);
+
+        $currentAnswer = (string) getGcd($firstOperand, $secondOperand);
+        $currentQuestion = "{$firstOperand} {$secondOperand}";
+
+        $gameData[] = [$currentQuestion, $currentAnswer];
+    }
+    runGame(DESCRIPTION, $gameData);
+}
