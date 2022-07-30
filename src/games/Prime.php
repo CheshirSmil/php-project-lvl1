29ad9ac386bcf2a3fc03a4d0c05b327cc24e2c46
Vladimir Ugarov <graciadan@rambler.ru>
2022-07-30 16:23:19 +0300

    add step 9

---
+++ src/games/Prime.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Brain\Games\Prime;
+
+use function Brain\Engine\runGame;
+
+use const Brain\Engine\ROUNDS_COUNT;
+
+const DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+function isPrime($num)
+{
+    if ($num <= 1) {
+        return false;
+    };
+
+    for ($i = 2; $i <= $num / 2; $i++) {
+        if ($num % $i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function run()
+{
+    $gameData = [];
+
+    for ($i = 0; $i < ROUNDS_COUNT; $i++) {
+        $randomNum = mt_rand(1, 100);
+        $currentAnswer = isPrime($randomNum) ? 'yes' : 'no';
+        $currentQuestion = (string) $randomNum;
+        $gameData[] = [$currentQuestion, $currentAnswer];
+    }
+    runGame(DESCRIPTION, $gameData);
+}
